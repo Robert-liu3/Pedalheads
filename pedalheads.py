deea1204d7715f1c60f6720b6450e3819fbd8e7b
Robert-liu3 <robertliu80@gmail.com>
2021-07-27 15:31:28 -0400

    added new UI page

--- pedalheads.py
+++ pedalheads.py
@@ -10,21 +10,48 @@ from functionF import *
 #C:\Users\Robert Liu\Desktop\list.xlsx
 #C:\Users\Robert Liu\Desktop\test.xlsx
 
+#variables for instructions
+instruction1 = [sg.Text("Welcome to the Pedalheads registration system!!! \nWhen typing in a file path, the format should be similar to C:\\Users\\Name\\Etc\\file_name")]
 
+#instruction2 = [sg.Text("When typing in a file path, the format should be similar to", r"C:\Users\Name\Etc\file_name")]
+
+nextButton = [sg.Button("NEXT")]
+
+layoutInstruction = [
+    [
+        instruction1,
+        #instruction2,
+        nextButton,
+    ]
+]
 #variables with names and buttons
 input_file_path1 = [sg.Text("INSERT FILE LOCATION"), sg.In(size=(55,1), enable_events=True, key = "-FILE1-")]
 #button1 = [sg.Button("Submit file")]
 input_file_path2 = [sg.Text("INSERT NEW FILE LOCATION"), sg.In(size=(50,1), enable_events=True, key = "-FILE2-")]
+
 button = [sg.Button("Generate file")]
 
 layout = [
     [
         input_file_path1,
-        input_file_path2, button,
+        input_file_path2, 
+        button,
     ]
 ]
+
 #variables that are windows
 window = sg.Window("AUTOMATIC REGISTRATION", layout, margins=(300,100))
+instructionWindow = sg.Window("BEGINNING",layoutInstruction, margins=(300,100))
+
+while True:
+    eventIn, valueIn = instructionWindow.read()
+    if eventIn == "NEXT":
+        break
+    if eventIn == sg.WIN_CLOSED:
+        exit()
+instructionWindow.close()
+
+
 while True:
     event, values = window.read()
     if event == "Generate file":
